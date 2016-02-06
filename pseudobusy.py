229ddd0f42639ba47f63032ad8217494b0e18e6b
Brandon Dreager <bmdreager@subol.es>
2016-02-06 01:05:18 -0500

    Better checks. Improved log system.

--- pseudobusy.py
+++ pseudobusy.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-from printer import Printer, Random
-from argparse import ArgumentParser
-
 __program__ = 'PseudoBusy'
 __version__ = '0.8.0'
 __description__ = 'Terminal vomit'
+
+import os
+from printer import Printer, Random
+from argparse import ArgumentParser
 
 class PseudoBusy():
     MAX_PATIENCE = 6
     MIN_FILE_LENGTH = 1
     MAX_FILE_LENGTH = 5000
+    MAX_FILE_SIZE = 100000000 # 100 MB
 
     def __init__(self, args=None):
         self.args = args
@@ -28,19 +29,22 @@ class PseudoBusy():
         # TODO whitelist and blacklist
         self.files = [os.path.join(path, filename) for path, dirs, files in os.walk(self.target_dir) for filename in files]
         self.original_num_files = len(self.files)
+
         self.printer.write('Found {} files'.format(self.original_num_files))
         self.run()
 
     def run(self):
         while self.running:
-            self.printer.reset()
-            infile, num_lines = self.pick_file()
+            try:
+                self.printer.reset()
+                infile, num_lines = self.pick_file()
 
-            if self.args.typing_speed: self.printer.override_speed = self.args.typing_speed
-            self.verbose("Reading: "+infile.replace(self.target_dir, '') if self.hide_target else infile)
-            self.verbose("\nLines: {}\n".format(num_lines))
-            self.verbose("Rejects: {}\n".format(self.original_num_files - len(self.files)))
-            self.print_file(infile)
+                if self.args.typing_speed: self.printer.override_speed = self.args.typing_speed
+                self.log(1, "Reading: "+infile.replace(self.target_dir, '') if self.hide_target else infile)
+                self.log(2, "\nLines:{}, Rejects:{}\n".format(num_lines,self.original_num_files - len(self.files)))
+                self.print_file(infile)
+            except KeyboardInterrupt:
+                self.running = False
 
     def print_file(self, infile):
         try:
@@ -59,47 +63,37 @@ class PseudoBusy():
                         self.printer.write(self.rand.string(num))
                         self.printer.backspace_delete(num)
                     self.printer.write(line)
-        except: pass
+        except Exception, err:
+            self.log(3, str(err)+'\n')
 
-    def pick_file(self):
-        file = num_lines = None
+    def pick_file(self, index=None):
+        file = num_lines =  None
         while not file:
             try:
-                file = self.rand.choice(self.files)
+                file = self.files[index] if index else self.rand.choice(self.files)
+                if os.path.getsize(file) >= self.MAX_FILE_SIZE: raise  Exception('File too large')
                 if not os.access(file, os.R_OK): raise Exception('No read access')
+                with open(file, 'r') as ins:
+                    ins.readline().decode('ascii')  # for catching junk we don't care to see
+                    num_lines = sum(1 for _ in ins) + 1
 
-                with open(file, 'r') as ins: ins.readline().decode('ascii')  # for catching junk we don't care to see
-                num_lines = self.bufcount(file)
-                if num_lines <= self.MIN_FILE_LENGTH: raise Exception('File too small') # for empty and single line files
-                if num_lines >= self.MAX_FILE_LENGTH: raise Exception('File too large') # for massive files (probably not code)
+                if num_lines <= self.MIN_FILE_LENGTH: raise Exception('Too few lines') # for empty and single line files
+                if num_lines >= self.MAX_FILE_LENGTH: raise Exception('Too many lines') # for massive files (probably not code)
             except Exception, err:
-                self.verbose(str(err)+'\n')
+                self.log(3, str(err)+'\n')
                 self.files.remove(file) # we don't need to see rejects again
                 file = None
 
         return (file, num_lines)
 
-    def verbose(self, string):
-        if self.args.verbose: self.printer.write(string)
-
-    @staticmethod
-    def bufcount(filename):
-        f = open(filename)
-        lines = 0
-        buf_size = 1024 * 1024
-        read_f = f.read
-
-        buf = read_f(buf_size)
-        while buf:
-            lines += buf.count('\n')
-            buf = read_f(buf_size)
-        return lines
+    def log(self, level, string):
+        if (level <= self.args.verbose): self.printer.write(string)
 
 def init_args():
     parser = ArgumentParser(prog=__program__, description=__description__)
     parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
-    parser.add_argument('-v', '--verbose', action='store_true', default=False,
-                        help='turn on verbose output', dest='verbose')
+    parser.add_argument('-v', '--verbose-level', type=int, default=1, choices=range(4),
+                        help='set verbose output level (default: %(default)s)', dest='verbose')
     parser.add_argument('-s', '--typing-speed-override', type=float, default=0,
                         help='overrides typing speed', dest='typing_speed')
 
@@ -110,8 +104,7 @@ if __name__ == '__main__':
     try:
         print Printer.CLEAR
         PseudoBusy(args=args).start()
-    except Exception, e:
-        print Printer.RESET
-        print Printer.CLEAR
-        print e
+    except:# Exception, e:
+        print Printer.CLEAR + Printer.RESET + Printer.CLEAR
+        #print e
         pass
