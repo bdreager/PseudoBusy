6c857d8567a1b5abef1a59d076b613b33a46490b
Brandon Dreager <bmdreager@subol.es>
2016-02-05 21:24:17 -0500

    Added a few command line arguments.

--- pseudobusy.py
+++ pseudobusy.py
@@ -3,13 +3,19 @@
 
 import os
 from printer import Printer, Random
+from argparse import ArgumentParser
+
+__program__ = 'PseudoBusy'
+__version__ = '0.8.0'
+__description__ = 'Terminal vomit'
 
 class PseudoBusy():
     MAX_PATIENCE = 6
     MIN_FILE_LENGTH = 1
     MAX_FILE_LENGTH = 5000
 
-    def __init__(self):
+    def __init__(self, args=None):
+        self.args = args
         self.rand = Random()
         self.printer = Printer(shift_in_chance=25)
         self.target_dir = os.path.expanduser('~/')
@@ -18,7 +24,7 @@ class PseudoBusy():
 
     def start(self):
         self.running = True
-        self.printer.write('Generating file list... ', speed=50)
+        self.printer.write('Generating file list... ', speed=1)
         # TODO whitelist and blacklist
         self.files = [os.path.join(path, filename) for path, dirs, files in os.walk(self.target_dir) for filename in files]
         self.original_num_files = len(self.files)
@@ -30,9 +36,10 @@ class PseudoBusy():
             self.printer.reset()
             infile, num_lines = self.pick_file()
 
-            #self.printer.override_speed = 1000 # NOTE just for testing
-            self.printer.write("Reading: "+infile.replace(self.target_dir, '') if self.hide_target else infile)
-            self.printer.write("\nLines: {}| Rejects: {} \n".format(num_lines, self.original_num_files - len(self.files)))
+            if self.args.typing_speed: self.printer.override_speed = self.args.typing_speed
+            self.verbose("Reading: "+infile.replace(self.target_dir, '') if self.hide_target else infile)
+            self.verbose("\nLines: {}\n".format(num_lines))
+            self.verbose("Rejects: {}\n".format(self.original_num_files - len(self.files)))
             self.print_file(infile)
 
     def print_file(self, infile):
@@ -40,6 +47,7 @@ class PseudoBusy():
             with open(infile, "r") as ins:
                 patience = self.MAX_PATIENCE
                 for line in ins:
+                    line = line.decode('ascii')  # hides garbage
                     if line.strip():
                         patience = self.MAX_PATIENCE
                     else:
@@ -65,11 +73,14 @@ class PseudoBusy():
                 if num_lines <= self.MIN_FILE_LENGTH: raise Exception('File too small') # for empty and single line files
                 if num_lines >= self.MAX_FILE_LENGTH: raise Exception('File too large') # for massive files (probably not code)
             except Exception, err:
-                # TODO print err if verbose
+                self.verbose(str(err)+'\n')
                 self.files.remove(file) # we don't need to see rejects again
                 file = None
 
         return (file, num_lines)
+
+    def verbose(self, string):
+        if self.args.verbose: self.printer.write(string)
 
     @staticmethod
     def bufcount(filename):
@@ -82,13 +93,23 @@ class PseudoBusy():
         while buf:
             lines += buf.count('\n')
             buf = read_f(buf_size)
-
         return lines
 
+def init_args():
+    parser = ArgumentParser(prog=__program__, description=__description__)
+    parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='turn on verbose output', dest='verbose')
+    parser.add_argument('-s', '--typing-speed-override', type=float, default=0,
+                        help='overrides typing speed', dest='typing_speed')
+
+    return parser.parse_args()
+
 if __name__ == '__main__':
+    args = init_args()
     try:
         print Printer.CLEAR
-        PseudoBusy().start()
+        PseudoBusy(args=args).start()
     except Exception, e:
         print Printer.RESET
         print Printer.CLEAR
